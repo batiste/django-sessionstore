b7c5600bc94bde47c26ca1ffa8df48f772ce0cf4
Cyril Doussin <cdoussin@opera.com>
2010-04-20 15:40:56 +0200

    add redis session backend

---
+++ djsession/backends/redis_db.py
@@ -0,0 +1,68 @@
+from cPickle import loads, dumps
+
+from django.contrib.sessions.backends.base import SessionBase, CreateError
+from django.conf import settings
+
+import redis
+
+
+r = redis.Redis(
+    host=getattr(settings, 'REDIS_SESSION_HOST', None),
+    port=getattr(settings, 'REDIS_SESSION_PORT', None),
+    socket_timeout=getattr(settings, 'REDIS_SESSION_SOCKET_TIMEOUT', None),
+    db=getattr(settings, 'REDIS_SESSION_DB', None))
+
+
+class SessionStore(SessionBase):
+    """
+    A redis-based session store.
+    """
+    def __init__(self, session_key=None):
+        self.redis = r 
+        # self.redis.connect()
+        super(SessionStore, self).__init__(session_key)
+
+    def load(self):
+        session_data = self.redis.get(self.session_key)
+        if session_data is not None:
+            return loads(session_data)
+        self.create()
+        return {}
+
+    def create(self):
+        while True:
+            self.session_key = self._get_new_session_key()
+            try:
+                self.save(must_create=True)
+            except CreateError:
+                # Would be raised if the key wasn't unique
+                continue
+            self.modified = True
+            return
+
+    def save(self, must_create=False):
+        # MULTI/EXEC command is disabled for the moment as it doesn't seem 
+        # to be support in stable versions of redis yet (as of 1.2.6)
+        # self.redis.execute_command('MULTI')
+        if must_create:
+            # preserve=True -> SETNX
+            result = self.redis.set(
+                self.session_key, dumps(self._get_session(no_load=must_create)), preserve=True)
+            if result == 0: # 0 == not created, 1 == created.
+                raise CreateError
+        else:
+            self.redis.set(self.session_key, dumps(self._get_session(no_load=must_create)),)
+        self.redis.execute_command('EXPIRE', self.session_key, getattr(settings, 'REDIS_SESSION_KEY_TTL', 60 * 60 * 24))
+        # self.redis.execute_command('EXEC')
+
+    def exists(self, session_key):
+        if self.redis.exists(session_key):
+            return True
+        return False
+
+    def delete(self, session_key=None):
+        if session_key is None:
+            if self._session_key is None:
+                return
+            session_key = self._session_key
+        self.redis.delete(session_key)
