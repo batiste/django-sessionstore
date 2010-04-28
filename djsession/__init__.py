1788031b6c855e65862d7460f0226ac77f5fded7
Cyril Doussin <cdoussin@opera.com>
2010-04-28 12:32:25 +0200

    New version version = (0, 0, 8)

--- djsession/__init__.py
+++ djsession/__init__.py
@@ -1,5 +1,5 @@
 """Django Cache+DB Session wich uses table rotation for expiry"""
-VERSION = (0, 0, 7)
+VERSION = (0, 0, 8)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
