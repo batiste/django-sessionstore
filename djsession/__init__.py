e8a8b05bcf9f902df7a7e46ab2f0ffba4a1df705
Batiste Bieler <batisteb@opera.com>
2010-11-25 12:55:15 +0100

    New version version = (0, 0, 15)

--- djsession/__init__.py
+++ djsession/__init__.py
@@ -1,5 +1,5 @@
 """Django Cache+DB Session wich uses table rotation for expiry"""
-VERSION = (0, 0, 14)
+VERSION = (0, 0, 15)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
